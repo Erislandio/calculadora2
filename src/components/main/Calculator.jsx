f1e236e7414f09ae1e4fcf320c085a6369945ad0
Erislandio <34255207+Erislandio@users.noreply.github.com>
2019-08-25 00:31:32 -0300

    erislandio
    AAdd type button add click event

--- src/components/main/Calculator.jsx
+++ src/components/main/Calculator.jsx
@@ -3,27 +3,39 @@ import Button from "../button/Button";
 import Display from "../display/Display";
 
 export default class Calculator extends React.Component {
+  clearMemory = () => {
+    console.log("limpar");
+  };
+
+  setOperation = operation => {
+    console.log(operation);
+  };
+
+  addDigit = n => {
+    console.log(n);
+  };
+
   render() {
     return (
       <div className="calculator">
         <Display value={100} />
-        <Button label="AC" triple />
-        <Button label="/" operation />
-        <Button label="7" />
-        <Button label="8" />
-        <Button label="9" />
-        <Button label="*" operation />
-        <Button label="4" />
-        <Button label="5" />
-        <Button label="6" />
-        <Button label="-" operation />
-        <Button label="1" />
-        <Button label="2" />
-        <Button label="3" />
-        <Button label="+" operation />
-        <Button label="0" double />
-        <Button label="." />
-        <Button label="=" operation />
+        <Button label="AC" triple click={() => this.clearMemory()} />
+        <Button label="/" operation click={this.setOperation} />
+        <Button label="7" click={this.addDigit} />
+        <Button label="8" click={this.addDigit} />
+        <Button label="9" click={this.addDigit} />
+        <Button label="*" operation click={this.setOperation} />
+        <Button label="4" click={this.addDigit} />
+        <Button label="5" click={this.addDigit} />
+        <Button label="6" click={this.addDigit} />
+        <Button label="-" operation click={this.setOperation} />
+        <Button label="1" click={this.addDigit} />
+        <Button label="2" click={this.addDigit} />
+        <Button label="3" click={this.addDigit} />
+        <Button label="+" operation click={this.setOperation} />
+        <Button label="0" double click={this.addDigit} />
+        <Button label="." click={this.addDigit} />
+        <Button label="=" operation click={this.setOperation} />
       </div>
     );
   }
