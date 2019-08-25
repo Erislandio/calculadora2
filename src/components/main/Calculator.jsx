6f5da23077ea9c4b767a435630204762c0b18632
Erislandio <34255207+Erislandio@users.noreply.github.com>
2019-08-25 01:08:48 -0300

    erislandio
    Finalização da logica

--- src/components/main/Calculator.jsx
+++ src/components/main/Calculator.jsx
@@ -20,11 +20,32 @@ export default class Calculator extends React.Component {
 
   clearMemory = () => {
     this.setState({ ...initialState });
-    console.log("limpar");
   };
 
   setOperation = operation => {
-    console.log(operation);
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true });
+    } else {
+      const equals = operation === "=";
+      const currentOperation = this.state.operation;
+
+      const values = [...this.state.values];
+
+      try {
+        values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`);
+      } catch (error) {
+        values[0] = this.state.values[0];
+      }
+      values[1] = 0;
+
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values
+      });
+    }
   };
 
   addDigit = n => {
@@ -61,7 +82,7 @@ export default class Calculator extends React.Component {
         <Button label="7" click={this.addDigit} />
         <Button label="8" click={this.addDigit} />
         <Button label="9" click={this.addDigit} />
-        <Button label="*" operation click={this.setOperation} />
+        <Button label="*" operation click={() => this.setOperation()} />
         <Button label="4" click={this.addDigit} />
         <Button label="5" click={this.addDigit} />
         <Button label="6" click={this.addDigit} />
