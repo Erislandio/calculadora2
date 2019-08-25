d91447c9cdf12d41aaeb02b6db7d774f7868c72e
Erislandio <34255207+Erislandio@users.noreply.github.com>
2019-08-24 23:59:45 -0300

    erislandio
    Add button component

--- src/components/main/Calculator.jsx
+++ src/components/main/Calculator.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Button from "../button/Button";
 
 export default class Calculator extends React.Component {
   render() {
-    return <div className="calculator">Calculator</div>;
+    return (
+      <div className="calculator">
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+      </div>
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@ import ReactDOM from "react-dom";
 import Calculator from "./components/main/Calculator";
 
 import "./styles.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Calculadora</h1>
+      <Calculator />
+    </div>
+  );
+};
+
 const rootElement = document.getElementById("root");
-ReactDOM.render(<Calculator />, rootElement);
+ReactDOM.render(<App />, rootElement);
