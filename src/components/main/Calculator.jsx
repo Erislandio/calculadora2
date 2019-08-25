bd09bba6e6e871092dbaa3383aa28c8cca101278
Erislandio <34255207+Erislandio@users.noreply.github.com>
2019-08-24 23:49:57 -0300

    Erislando
    Add Calculator Component

---
+++ src/components/main/Calculator.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class Calculator extends React.Component {
+  render() {
+    return <div className="calculator">Calculator</div>;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Calculator from "./components/main/Calculator";
 
 import "./styles.css";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello Teste 3</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
-
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Calculator />, rootElement);
